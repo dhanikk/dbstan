cf00c128aad7d51dea34920dd618e0e651b62edc
Jasper <jasper@dedact.nl>
2026-03-31 10:14:32 +0200

    Add fallback to the current default in the config method

--- src/DBStanAnalyzer.php
+++ src/DBStanAnalyzer.php
@@ -24,7 +24,7 @@ class DBStanAnalyzer
     public function getPreflightError(): ?string
     {
         $databaseName = DB::getDatabaseName();
-        $migrationTableName = config('database.migrations');
+        $migrationTableName = config('database.migrations', 'migration');
 
         if (empty($databaseName)) {
             return 'Database is not configured. Please set DB connection values in your .env file and try again.';
